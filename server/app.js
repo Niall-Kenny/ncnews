acadb837029bef6cc7dc9140dadef94ac979b564
Niall Kenny <NiallKenny@Nialls-MacBook-Pro.local>
2019-08-05 09:58:17 +0100

    added cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
+const cors = require('cors')
 const {
   routeError,
   customError,
@@ -8,6 +9,7 @@ const {
 } = require("./error_handlers/errors");
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api", apiRouter);
 
